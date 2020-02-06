0563ddeddf721826a2ab5ee8bfd89fa9880d3ab7
sergBull <bullsergv@gmail.com>
2020-02-06 10:12:17 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 	echo "<meta charset='utf-8'/>";
-	echo "<h1>B<h2>A#</h2>A<h2>G#</h2>G<h2>F#</h2>FE<h2>D#</h2>D<h2>C#</h2>C</h1>";
+	echo "<h1>B<h2>A#</h2>A<h2>G#</h2>G<h2>F#</h2>F<br/>E<h2>D#</h2>D<h2>C#</h2>C</h1>";
 ?>
