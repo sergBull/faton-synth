dad4b0c288922bdbf60c437fa4f29b964ea3d348
sergBull <bullsergv@gmail.com>
2020-02-06 10:28:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+	echo "<link href='styles.css' rel='stylesheet'/>";
 	echo "<meta charset='utf-8'/>";
-	echo "<h1>B<h2>A#</h2>A<h2>G#</h2>G<h2>F#</h2>F<br/>E<h2>D#</h2>D<h2>C#</h2>C</h1>";
+	echo "<button>C</button>".
+		"<button>C#</button>".
+		"<button>D</button>".
+		"<button>D#</button>".
+		"<button>E</button>".
+		"<button>F</button>".
+		"<button>F#</button>".
+		"<button>G</button>".
+		"<button>G#</button>".
+		"<button>A</button>".
+		"<button>A#</button>".
+		"<button>B</button>";
 ?>
